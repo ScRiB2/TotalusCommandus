9e692d8b1aab6479a192b0dedab95981071812f0
AlexanderNesterov <aleksandr_nesterov_99@mail.ru>
2019-11-29 18:56:53 +0300

    add save and load logic

--- TotalCommander/MainWindow.xaml.cs
+++ TotalCommander/MainWindow.xaml.cs
@@ -16,6 +16,8 @@ using System.IO;
 using TotalCommander.MainViews;
 using TotalCommander.Classes;
 using TotalCommander.AdditionalElements;
+using System.Windows.Markup;
+using Microsoft.Win32;
 
 namespace TotalCommander
 {
@@ -31,6 +33,7 @@ namespace TotalCommander
 
         private SolidColorBrush fontColor;
         private SolidColorBrush backgroundColor;
+        private int fontSize;
         private enum Colors
         {
             Default,
@@ -98,7 +101,7 @@ namespace TotalCommander
             this.sideRight.RefreshList();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void applyFontSize_Click(object sender, RoutedEventArgs e)
         {
             bool fuck = false;
             for (int i = 0; i < fontSizeBox.Text.Length; i++)
@@ -116,21 +119,93 @@ namespace TotalCommander
 
                 if (size < 10 || size > 20)
                 {
-                    sideLeft.changeFont(15);
-                    sideRight.changeFont(15);
+                    fontSize = 15;
+                    sideLeft.changeFont(fontSize);
+                    sideRight.changeFont(fontSize);
+                    return;
                 }
 
+                fontSize = size;
                 sideLeft.changeFont(size);
                 sideRight.changeFont(size);
             }
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private void applyStyle_Click(object sender, RoutedEventArgs e)
         {
             if (fontColor != null && backgroundColor != null)
             {
                 sideLeft.chageColor(fontColor, backgroundColor);
                 sideRight.chageColor(fontColor, backgroundColor);
+            }
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "dat files (*.dat)|*.dat";
+            dialog.ShowDialog();
+
+            string path = dialog.FileName;
+
+            if (path.Equals(""))
+            {
+                return;
+            }
+
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                MyStyle style = new MyStyle();
+                if (fontColor == null || backgroundColor == null)
+                {
+                    style.fontColor = Brushes.Blue;
+                    style.backgroundColor = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFE3F8FF");
+                }
+                else
+                {
+                    style.fontColor = fontColor;
+                    style.backgroundColor = backgroundColor;
+                }
+
+                if (fontSize < 10 || fontSize > 20)
+                {
+                    style.fontSize = 15;
+                }
+                else
+                {
+                    style.fontSize = fontSize;
+                }
+                
+                string styleStr = XamlWriter.Save(style);
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(styleStr);
+                sw.Close();
+                fs.Close();
+            }
+        }
+
+        private void load_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "dat files (*.dat)|*.dat";
+            dialog.ShowDialog();
+
+            string path = dialog.FileName;
+
+            if (path.Equals(""))
+            {
+                return;
+            }
+
+            using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                StreamReader sr = new StreamReader(fs);
+                MyStyle style = (MyStyle)XamlReader.Parse(sr.ReadToEnd());
+
+                sideLeft.chageColor(style.fontColor, style.backgroundColor);
+                sideRight.chageColor(style.fontColor, style.backgroundColor);
+                sideLeft.changeFont(style.fontSize);
+                sideRight.changeFont(style.fontSize);
             }
         }
     }
